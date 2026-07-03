4cfa1561cb682dcc74b9218241dd94f5247b45e6
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2026-07-03 16:06:17 +0200

    feat: add responsive site header with mobile menu and scroll effects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
       <div className="header-inner">
         <div className="header-logo" >
           <a href="/">
-            <img src="/images/logo-hyria.png" alt="Hyria Basket" style={{ height: '60px' }} />
+            <img src="/images/logo-hyria.png" alt="Hyria Basket" className="h-10 md:h-12 w-auto object-contain" />
           </a>
         </div>
 
